d5e7640d259820c18f7cb367aeb24f3318499869
Michael Baker <polyphonic@hotmail.com>
2026-05-01 22:59:00 -0700

    bye bye rockets

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,6 @@ ROTO_CATEGORIES = [
 # Used by the owner page to gray out players whose teams are no longer playing.
 # Stats for eliminated players still count toward season totals.
 ACTIVE_TEAMS: set = {
-    "BOS", "CLE", "DET", "HOU", "LAL", 
+    "BOS", "CLE", "DET", "LAL", 
     "MIN", "NYK", "OKC", "ORL", "PHI", "SAS", "TOR",
 }
