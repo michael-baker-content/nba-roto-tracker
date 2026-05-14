2f44ca2840b833f1ed250d020f552881f176ce65
Michael Baker <polyphonic@hotmail.com>
2026-05-13 17:47:07 -0700

    bye lakers and sixers

--- config/settings.py
+++ config/settings.py
@@ -74,5 +74,4 @@ ROTO_CATEGORIES = [
 # Used by the owner page to gray out players whose teams are no longer playing.
 # Stats for eliminated players still count toward season totals.
 ACTIVE_TEAMS: set = {
-    "CLE", "DET", "LAL", 
-    "MIN", "NYK", "OKC", "PHI", "SAS",}
+    "CLE", "DET", "MIN", "NYK", "OKC", "SAS",}
